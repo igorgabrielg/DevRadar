48660acd6df9a40e50134d597ba58c94f107555b
Igor Gabriel <igor.gabrielg@gmail.com>
2020-02-04 21:59:11 -0300

    Add comentarios sobre id root

--- web/src/index.js
+++ web/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Add na tag que tiver ID root os elementos de App.js
 ReactDOM.render(<App />, document.getElementById('root'));
